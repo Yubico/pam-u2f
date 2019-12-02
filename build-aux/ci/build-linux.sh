df10902c127b4710cd57dd5356f19a0ac2571429
Alessio Di Mauro <alessio@yubico.com>
2019-12-02 17:05:32 +0100

    Don't use `&&` together with `set -e` in the CI scripts

--- build-aux/ci/build-linux.sh
+++ build-aux/ci/build-linux.sh
@@ -4,5 +4,7 @@ set -ex
 BUILDROOT="$(git rev-parse --show-toplevel)"
 
 pushd "$BUILDROOT" &>/dev/null
-  ./autogen.sh && ./configure --disable-silent-rules --disable-man && make check
+  ./autogen.sh
+  ./configure --disable-silent-rules --disable-man
+  make check
 popd &>/dev/null
