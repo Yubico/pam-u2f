ca88b78fc0bfd6a7c3127c677cc2a4d390f56879
Alessio Di Mauro <alessio@yubico.com>
2019-12-04 09:00:34 +0100

    Remove "set -x" before calling clang_format
    It makes the otput more readable if there are formatting errors

--- build-aux/ci/build-bionic-gcc7.sh
+++ build-aux/ci/build-bionic-gcc7.sh
@@ -3,5 +3,8 @@ set -ex
 
 BUILDROOT="$(git rev-parse --show-toplevel)"
 
+set +x
 source $BUILDROOT/build-aux/ci/format-code.sh "$(git rev-parse HEAD~)"
+set -x
+
 source $BUILDROOT/build-aux/ci/build-linux.sh
