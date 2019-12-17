83fe353543ce643d07399338dfdb9c4fe79ee784
Alessio Di Mauro <alessio@yubico.com>
2019-12-17 15:41:10 +0100

    Use calloc to allocate buf

--- util.c
+++ util.c
@@ -938,7 +938,7 @@ int get_devices_from_authfile(const cfg_t *cfg, const char *username,
     fd = -1; /* fd belongs to opwfile */
   }
 
-  buf = malloc(sizeof(char) * (DEVSIZE * cfg->max_devs));
+  buf = calloc(1, sizeof(char) * (DEVSIZE * cfg->max_devs));
   if (!buf) {
     if (cfg->debug)
       D(cfg->debug_file, "Unable to allocate memory");
