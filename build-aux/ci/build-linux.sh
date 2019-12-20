2560e3a759792732f334292983a00226a20c01d2
Alessio Di Mauro <alessio@yubico.com>
2019-12-20 11:05:23 +0100

    Change ownership of the test credential files

--- build-aux/ci/build-linux.sh
+++ build-aux/ci/build-linux.sh
@@ -4,6 +4,7 @@ set -ex
 BUILDROOT="$(git rev-parse --show-toplevel)"
 
 pushd "$BUILDROOT" &>/dev/null
+  sudo chown root:root tests/credentials/*
   ./autogen.sh
   ./configure --disable-silent-rules --disable-man
   make check
