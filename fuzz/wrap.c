65629cda076beddcae475b9ac3063fdabb57298d
Ludvig Michaelsson <ludvig.michaelsson@yubico.com>
2022-02-21 12:09:40 +0100

    fuzz: sort includes

--- fuzz/wrap.c
+++ fuzz/wrap.c
@@ -1,13 +1,13 @@
 /* Copyright (C) 2021 Yubico AB - See COPYING */
-#include <assert.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <pwd.h>
-#include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include <openssl/bio.h>
