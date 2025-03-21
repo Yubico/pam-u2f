ab668acacdaf81fd4ced6847deddcd62409d4bd4
Giovanni Simoni <giovanni.simoni@yubico.com>
2025-03-21 13:36:50 +0100

    fuzz: enable fuzzing reports
    Generate the file-level coverage summary report (`llvm-cov report`) and print
    it on stdout.
    
    Optionally generate the line-oriented coverage report (`llvm-cov show`).
    This is enabled if the WITH_REPORTS environment variable is defined.
    The REPORTS_OUTPUT_DIR environment variable must be set to the output
    directory.

--- build-aux/ci/fuzz-linux-asan.sh
+++ build-aux/ci/fuzz-linux-asan.sh
@@ -6,6 +6,8 @@ set -eux
 
 CORPUS_URL="https://storage.googleapis.com/yubico-pam-u2f/corpus.tgz"
 
+WORKDIR="${WORKDIR:-$(pwd)}"
+
 LIBCBOR_URL="https://github.com/pjk/libcbor"
 LIBCBOR_TAG="v0.11.0"
 LIBCBOR_CFLAGS="-fsanitize=address,alignment,bounds"
@@ -18,11 +20,12 @@ PAM_U2F_CFLAGS="-fsanitize=address,pointer-compare,pointer-subtract"
 PAM_U2F_CFLAGS="${PAM_U2F_CFLAGS},undefined,bounds"
 PAM_U2F_CFLAGS="${PAM_U2F_CFLAGS},leak"
 PAM_U2F_CFLAGS="${PAM_U2F_CFLAGS} -fno-sanitize-recover=all"
+PAM_U2F_CFLAGS="${PAM_U2F_CFLAGS} -fprofile-instr-generate -fcoverage-mapping"
+PAM_U2F_CFLAGS="${PAM_U2F_CFLAGS} -fcoverage-compilation-dir=$WORKDIR"
 
 NPROC="$(nproc)"
 
 ${CC} --version
-WORKDIR="${WORKDIR:-$(pwd)}"
 
 if [ -n "${FAKEROOT:-}" ]; then
 	mkdir -p "${FAKEROOT}"
@@ -30,6 +33,18 @@ if [ -n "${FAKEROOT:-}" ]; then
 else
 	FAKEROOT="$(mktemp -d)"
 	trap 'rm -rf "$FAKEROOT"' 0
+fi
+
+if [ -n "${WITH_COVERAGE_REPORTS:-}" ]; then
+	REPORTS_OUTPUT_DIR="$(
+		mkdir -p "${REPORTS_OUTPUT_DIR:?}"
+		cd "$REPORTS_OUTPUT_DIR"
+		pwd
+	)"
+
+	export REPORTS_OUTPUT_DIR
+	export WITH_COVERAGE_REPORTS
+
 fi
 
 export LD_LIBRARY_PATH="${FAKEROOT}/lib"
@@ -88,7 +103,39 @@ cmake --build build.pam_u2f -j "$NPROC"
 # fuzz
 curl --retry 4 -s -o corpus.tgz "${CORPUS_URL}"
 tar xzf corpus.tgz
-build.pam_u2f/fuzz/fuzz_format_parsers corpus/format_parsers \
-	-reload=30 -print_pcs=1 -print_funcs=30 -timeout=10 -runs=1
-build.pam_u2f/fuzz/fuzz_auth corpus/auth \
-	-reload=30 -print_pcs=1 -print_funcs=30 -timeout=10 -runs=1
+
+run_fuzzer() (
+	export LLVM_PROFILE_FILE="$FAKEROOT/$FUZZER_NAME.profraw"
+
+	FUZZER_BINARY="$FAKEROOT/build.pam_u2f/fuzz/fuzz_$FUZZER_NAME"
+	CORPUS="$FAKEROOT/corpus/$FUZZER_NAME"
+	PROFILE_MERGED="${LLVM_PROFILE_FILE%.*}.profdata"
+
+	"$FUZZER_BINARY" "$CORPUS" \
+		-reload=30 \
+		-print_pcs=1 \
+		-print_funcs=30 \
+		-timeout=10 \
+		-runs=1
+
+		llvm-profdata merge \
+			-sparse "$LLVM_PROFILE_FILE" \
+			-o "$PROFILE_MERGED"
+
+	if [ -n "${WITH_COVERAGE_REPORTS:-}" ]; then
+		llvm-cov show \
+			-o "$REPORTS_OUTPUT_DIR/$FUZZER_NAME.report.d" \
+			-format=html \
+			"$FAKEROOT/build.pam_u2f/fuzz/libpam_u2f_fuzz.so" \
+			-instr-profile "$PROFILE_MERGED" \
+			-sources "$WORKDIR"
+	fi
+
+	llvm-cov report --show-branch-summary=false \
+		"$FAKEROOT/build.pam_u2f/fuzz/libpam_u2f_fuzz.so" \
+		-instr-profile "$PROFILE_MERGED" \
+		-sources "$WORKDIR"
+)
+
+FUZZER_NAME=format_parsers run_fuzzer
+FUZZER_NAME=auth           run_fuzzer
